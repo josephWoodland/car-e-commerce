83b73ed0edbf02f8cbd3a50e79546ec0d235df1a
Joseph <joseph@workit-app.com>
2021-11-23 21:21:42 +0000

    Added the postgres database

--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,23 @@ def home(request):
     template = 'pages/home.html'
 
     return render(request, template)
+
+
+def about(request):
+    
+    template = 'pages/about.html'
+
+    return render(request, template)
+
+def services(request):
+    
+    template = 'pages/services.html'
+
+    return render(request, template)
+
+
+def contact(request):
+    
+    template = 'pages/contact.html'
+
+    return render(request, template)
