02757babf0513c3c1d41ae832f3eb3da8c17b259
Joseph <joseph@workit-app.com>
2021-11-24 14:24:55 +0000

    Added the admin views for the cars and the homepage carosel

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Car
+from django.utils.html import format_html
 
 # Register your models here.
-admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):
+
+
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="60px" style="border-radius: 60px"/>'.format(object.car_photo.url))
+
+    thumbnail.short_description = 'Car Image'
+
+    list_display = ('id', 'car_title', 'thumbnail', 'city', 'color', 'year', 'body_style', 'fuel_type', 'is_featured')
+    list_display_links = ('id', 'thumbnail', 'car_title')
+    list_editable = ('is_featured',)
+    search_fields = ('id', 'car_title', 'city', 'model', 'body_style', 'fuel_type')
+    list_filter = ('city', 'model', 'body_style', 'fuel_type')
+
+admin.site.register(Car, CarAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import Team
+from cars.models import Car
 # Create your views here.
 
 
 def home(request):
+
     teams = Team.objects.all()
+    feature_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    
     context = {
-        'teams': teams
+        'teams': teams,
+        'featured_cars': feature_cars,
     }
 
     template = 'pages/home.html'
