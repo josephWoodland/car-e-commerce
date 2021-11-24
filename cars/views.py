2e7ba4da19bc50c3676edc45648d63698f70695e
Joseph <joseph@workit-app.com>
2021-11-24 21:35:01 +0000

    Added the car_detials page content

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from .models import Car
 # Create your views here.
 
 def cars(request):
@@ -8,3 +8,15 @@ def cars(request):
     template = 'cars/cars.html'
 
     return render(request, template)
+
+
+def car_detail(request, id):
+
+    single_car = get_object_or_404(Car, pk=id)
+    template = 'cars/car_detail.html'
+    context = {
+        'car': single_car,
+
+    }
+    
+    return render(request, template, context)
