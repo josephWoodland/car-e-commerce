449879b7989f40f79d622c072e27da26254ee387
Joseph <joseph@workit-app.com>
2021-11-25 19:16:55 +0000

    Inserted the car class into the cars.html

--- cars/views.py
+++ cars/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Car
+
 # Create your views here.
 
 def cars(request):
-    
-
+    car_list = Car.objects.order_by('-created_date')
     template = 'cars/cars.html'
+    context = {
+        'cars': car_list,
 
-    return render(request, template)
+    }
+
+    return render(request, template, context)
 
 
 def car_detail(request, id):
 
     single_car = get_object_or_404(Car, pk=id)
-    template = 'cars/car_detail.html'
+    template = 'cars/car_detail.html'    
     context = {
         'car': single_car,
 
