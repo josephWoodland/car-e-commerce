b40f1609b7601ec0715e465fcc1f7b89a79fc428
Joseph <joseph@workit-app.com>
2021-11-25 20:08:28 +0000

    Added the pagination function to the cars page

--- cars/views.py
+++ cars/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Car
 
 # Create your views here.
 
 def cars(request):
     car_list = Car.objects.order_by('-created_date')
+    paginator = Paginator(car_list, 4 )
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+
     template = 'cars/cars.html'
     context = {
-        'cars': car_list,
-
+        'cars': paged_cars,
     }
 
     return render(request, template, context)
@@ -20,7 +24,13 @@ def car_detail(request, id):
     template = 'cars/car_detail.html'    
     context = {
         'car': single_car,
-
     }
     
     return render(request, template, context)
+
+
+def search(request):
+
+    template = 'cars/search.html'
+
+    return render(request, template)
