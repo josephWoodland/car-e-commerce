98eb0a3e44618e3dc8cb81fdfa6022663b5db6e7
Joseph <joseph@workit-app.com>
2021-12-04 14:53:17 +0000

    Added the search function to the search banner in search.html

--- carzone/settings.py
+++ carzone/settings.py
@@ -128,6 +128,7 @@ USE_TZ = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static/'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from cars.views import search
 from .models import Team
 from cars.models import Car
 # Create your views here.
@@ -9,11 +11,21 @@ def home(request):
     teams = Team.objects.all()
     feature_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by('-created_date')
+
+    # Saving all the unique search terms to add to the drop down list(instead of listing repeated terms)
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
     
     context = {
         'teams': teams,
         'featured_cars': feature_cars,
         'all_cars': all_cars,
+        'model_search' : model_search,
+        'city_search' : city_search,
+        'body_style_search' : body_style_search,
+        'year_search' : year_search,
     }
 
     template = 'pages/home.html'
